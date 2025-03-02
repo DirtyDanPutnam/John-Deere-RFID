812f1a301fe2deae2d5d3176dc3fe1d3addf1fc6
DirtyDanPutnam <daniel.putnam@ndus.edu>
2025-03-02 09:37:48 -0600

    stuff

--- DatabaseActions.java
+++ DatabaseActions.java
@@ -1,4 +1,5 @@
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DatabaseActions {
@@ -11,15 +12,44 @@ public class DatabaseActions {
         pstmt.setString(1, pd.epc);
         pstmt.setString(2, pd.logicalDevice);
         pstmt.setString(3, pd.baseLogicalDevice);
-        pstmt.setString(4, pd.rssi);
-        pstmt.setString(5, pd.timeUTC);
-        pstmt.setString(6, pd.latitude);
-        pstmt.setInt(7, pd.longitude);
-        pstmt.setBoolean(8, pd.antenna);
+        pstmt.setDouble(4, pd.rssi);
+        pstmt.setInt(5, pd.timeUTC);
+        pstmt.setDouble(6, pd.latitude);
+        pstmt.setDouble(7, pd.longitude);
+        pstmt.setInt(8, pd.antenna);
         
         int count = pstmt.executeUpdate();
 
         return count;
     }
 
+    public static int PullLastPingNum(PingData pd){
+        String query = "SELECT antenna_num FROM ping_data WHERE ping_id IN (SELECT antenna_last_ping FROM tool WHERE epc = ?)";
+
+        PreparedStatement pstmt = DatabaseManager.getConnection().prepareStatement(query);
+
+        pstmt.setString(1, pd.epc);
+
+        ResultSet rs = pstmt.executeQuery();
+
+        int pingNum = rs.getInt("antenna_num");
+
+        return pingNum;
+    }
+
+    public static int PullLastPingUTC(PingData pd){
+        String query = "SELECT timeutc FROM ping_data WHERE ping_id IN (SELECT antenna_last_ping FROM tool WHERE epc = ?)";
+
+        PreparedStatement pstmt = DatabaseManager.getConnection().prepareStatement(query);
+
+        pstmt.setString(1, pd.epc);
+
+        ResultSet rs = pstmt.executeQuery();
+
+        int pingUTC = rs.getInt("antenna_num");
+
+        return pingUTC;
+    }
+
+    //Insert tool?
 }
